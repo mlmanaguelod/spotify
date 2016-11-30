818665e4cf9c6066d91dbb5a8423c4157943f146
ML Managuelod <MLManaguelod@MLManaguelod-MacBook-Pro.local>
2016-11-30 17:14:24 +0800

    adding imagesss

--- index.php
+++ index.php
@@ -12,9 +12,28 @@
 			<div class="logo">
 				<img src="img/spotify.png" alt="Spotify Logo">
 			</div>
-			
+			<ul class="navigation">
+				<li><a href="#">Premium</a></li>
+				<li><a href="#">Help</a></li>
+				<li><a href="#">Download</a></li>
+				<li><a href="#">Sign Up</a></li>
+				<li><a href="#">Login</a></li>
+			</ul>
 		</div>	
 	</header>
+
+	<div class="banner">
+		<div class="container">
+			<div class="banner-contents">
+				<h1>Captivate senses.<br>Go PREMIUM</h1>
+				<h4>Listen to your favorite band for &#8369 9/month.</h4>
+
+				<!-- <a href="" class="purchase-btn">Purchase Premium for P9/month</a> -->
+				<button class="purchase-btn"><a href="#">Purchase Premium for P9/month</a></button>
+				<p>* Only &#8369 9/month after. Offer not available to users who already tried Premium. <a href="#">Terms Apply.</a></p>
+			</div>
+		</div>
+	</div>
 
 
 </body>
