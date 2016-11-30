ab9d23abf35c1f50ba987b5ce999bf34e6771a8e
ML Managuelod <MLManaguelod@MLManaguelod-MacBook-Pro.local>
2016-11-30 16:56:56 +0800

    adding the css this time, also img folder

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Music is for everyone - Spotify</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div class="logo">
+				<img src="img/spotify.png" alt="Spotify Logo">
+			</div>
+			
+		</div>	
+	</header>
+
+
+</body>
+</html>
